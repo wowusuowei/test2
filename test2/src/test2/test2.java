e202b0208e2da925a373ca3b3ec67deeae19a764
wb\root <wowusuowe3610@gmail.com>
2018-10-26 14:55:18 +0800

    test2

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -4,6 +4,7 @@ public class test2 {
 	public static void main(String[] args) {
 		System.out.println("hello Java");
 		System.out.println("2222");
+		System.out.println("3333");
 		System.out.println("4444");
 	}
 }
